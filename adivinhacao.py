15992518c80616d94bb2ed9f76e26cfcf5d552ff
John Laff <joaoaraxaiba@gmail.com>
2023-04-21 19:25:21 -0300

    Formatando  o código
    O código foi formatado com o formatador "black".

--- adivinhacao.py
+++ adivinhacao.py
@@ -11,10 +11,10 @@ for rodada in range(1, total_de_tentativas + 1):
     print(f"Você digitou {chute_str}")
     chute = int(chute_str)
 
-    if(chute < 1) or (chute > 100):
+    if (chute < 1) or (chute > 100):
         print("Você deve digitar um número entre 1 e 100!")
         continue
-    
+
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
