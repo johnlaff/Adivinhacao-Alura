3855a4af022c9c9d7bddc2c4fd3232aee10affa4
johnlaff <joaoaraxaiba@gmail.com>
2023-08-19 23:26:12 -0300

    Implementação do game loop e verificação de acertos
    A função “jogar” foi modificada para implementar a lógica do jogo da forca. Agora, a palavra secreta é definida como “banana” e o jogo entra em um loop no qual o jogador pode fazer chutes para adivinhar as letras da palavra. Cada chute é verificado e comparado com as letras da palavra secreta. Se houver alguma correspondência, é exibida a posição do chute. O loop permanece até que o jogador acerte todas as letras ou erre o suficiente para ser considerado “enforcado”. Ao final do game loop, seja por acerto ou erro, a mensagem “Fim do jogo” é exibida, encerrando a execução do jogo.

--- forca.py
+++ forca.py
@@ -3,7 +3,25 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+
+        chute = input("Qual letra?")
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute == letra:
+                print(f"Encontrei a letra {letra} na posição {index}")
+            index = index + 1
+
+        print("Jogando...")
+
     print("Fim do jogo")
 
-if(__name__ == "__main__"):
-        jogar()
+
+if __name__ == "__main__":
+    jogar()
