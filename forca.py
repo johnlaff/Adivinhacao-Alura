f7361cc32c01746b48aaa7c598481eae581daa9d
johnlaff <joaoaraxaiba@gmail.com>
2023-08-27 22:09:41 -0300

    Aleatorizando seleção de palavras
    Neste commit, aprimorou-se o jogo ao implementar a seleção da palavra secreta
    a partir do arquivo "palavras.txt". Isso proporciona uma experiência de jogo
    mais variada e envolvente. As palavras são lidas do arquivo e armazenadas na
    lista "palavras".
    
    A inclusão do arquivo "palavras.txt" enriquece o jogo, permitindo a utilização
    de palavras diversas a cada partida.

--- forca.py
+++ forca.py
@@ -1,10 +1,24 @@
+import random
+
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana".lower()
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].lower()
+
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
