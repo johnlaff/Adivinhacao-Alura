fac46b5b03029941f620e234563096bf3cf60ea6
johnlaff <joaoaraxaiba@gmail.com>
2023-08-29 13:16:43 -0300

    Refatoração e melhorias na lógica do jogo
    Este commit traz melhorias significativas na função "jogar" do jogo da forca.
    A seguir estão as principais mudanças:
    
    - Introdução de funções específicas para diferentes partes da lógica do jogo,
      como "boas_vindas", "carrega_palavra_secreta", "inicializa_letras_acertadas",
      "pede_chute", "marca_chute_correto", "imprime_mensagem_vencedor" e
      "imprime_mensagem_perdedor". Isso torna o código mais modular e de fácil
      manutenção.
    - Reorganização das chamadas de função no corpo da função "jogar", resultando
      em um código mais limpo e legível.
    - Simplificação das verificações de acerto e erro, deixando o código mais
      conciso e compreensível.
    
    Essas mudanças melhoram a estrutura e a clareza do código, tornando-o mais
    legível e modular. A inclusão de funções específicas facilita futuras
    manutenções e ajustes.

--- forca.py
+++ forca.py
@@ -2,10 +2,44 @@ import random
 
 
 def jogar():
+
+    boas_vindas()
+    palavra_secreta = carrega_palavra_secreta()
+
+    letras_acertadas = inicaliza_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    while not enforcou and not acertou:
+
+        chute = pede_chute()
+
+        if chute in palavra_secreta:
+            marca_chute_correto(chute, letras_acertadas, palavra_secreta)
+        else:
+            erros += 1
+            print(f"Você errou! Restam {6 - erros} tentativas.")
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        imprime_mensagem_vencedor()
+    else:
+        imprime_mensagem_perdedor()
+
+
+def boas_vindas():
     print("*********************************")
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
+
+def carrega_palavra_secreta():
     arquivo = open("palavras.txt", "r")
     palavras = []
 
@@ -17,38 +51,33 @@ def jogar():
 
     numero = random.randrange(0, len(palavras))
     palavra_secreta = palavras[numero].lower()
+    return palavra_secreta
 
-    letras_acertadas = ["_" for letra in palavra_secreta]
 
-    enforcou = False
-    acertou = False
-    erros = 0
+def inicaliza_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
 
-    print(letras_acertadas)
 
-    while not enforcou and not acertou:
+def pede_chute():
+    chute = input("Qual letra? ")
+    chute = chute.strip().lower()
+    return chute
 
-        chute = input("Qual letra? ")
-        chute = chute.strip().lower()
-        if chute in palavra_secreta:
-            index = 0
-            for letra in palavra_secreta:
-                if chute == letra:
-                    letras_acertadas[index] = letra
-                index += 1
-        else:
-            erros += 1
-            print(f"Você errou! Restam {6 - erros} tentativas.")
 
-        enforcou = erros == 6
-        acertou = "_" not in letras_acertadas
-        print(letras_acertadas)
+def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if chute == letra:
+            letras_acertadas[index] = letra
+        index += 1
 
-    if acertou:
-        print("Você ganhou!")
-    else:
-        print("Você perdeu!")
-    print("Fim do jogo")
+
+def imprime_mensagem_vencedor():
+    print("Você ganhou!")
+
+
+def imprime_mensagem_perdedor():
+    print("Você perdeu!")
 
 
 if __name__ == "__main__":
