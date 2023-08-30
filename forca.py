215a100031e71bbd7c88aa323db44b306582f055
johnlaff <joaoaraxaiba@gmail.com>
2023-08-30 14:28:35 -0300

    Melhorias na interface do jogo
    Este commit traz melhorias significativas na função "jogar" do jogo da forca.
    Foram introduzidas alterações para melhorar a experiência do usuário e tornar
    a interface do jogo mais amigável. As principais mudanças incluem:
    
    - Adição de mensagens visuais para o jogador saber o que aconteceu ao final do
      jogo, seja por vitória ou derrota.
    - Inclusão de uma função "desenha_forca" para desenhar a forca de acordo com o
      número de erros cometidos.
    - Melhorias na legibilidade do código, como espaçamento entre as partes do jogo
      e organização das funções.
    - O arquivo de palavras "palavras.txt" agora é decodificado em UTF-8, garantindo
      compatibilidade com caracteres Unicode.
    
    Essas mudanças melhoram a experiência do usuário ao jogar o jogo da forca,
    adicionando elementos visuais que tornam o jogo mais envolvente e emocionante.

--- forca.py
+++ forca.py
@@ -2,7 +2,6 @@ import random
 
 
 def jogar():
-
     boas_vindas()
     palavra_secreta = carrega_palavra_secreta()
 
@@ -21,16 +20,16 @@ def jogar():
             marca_chute_correto(chute, letras_acertadas, palavra_secreta)
         else:
             erros += 1
-            print(f"Você errou! Restam {6 - erros} tentativas.")
+            desenha_forca(erros)
 
-        enforcou = erros == 6
+        enforcou = erros == 7
         acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
     if acertou:
         imprime_mensagem_vencedor()
     else:
-        imprime_mensagem_perdedor()
+        imprime_mensagem_perdedor(palavra_secreta)
 
 
 def boas_vindas():
@@ -40,7 +39,7 @@ def boas_vindas():
 
 
 def carrega_palavra_secreta():
-    arquivo = open("palavras.txt", "r")
+    arquivo = open("palavras.txt", "r", encoding="utf-8")
     palavras = []
 
     for linha in arquivo:
@@ -73,11 +72,89 @@ def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
 
 
 def imprime_mensagem_vencedor():
-    print("Você ganhou!")
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
 
 
-def imprime_mensagem_perdedor():
-    print("Você perdeu!")
+def imprime_mensagem_perdedor(palavra_secreta):
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}.".format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if (erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if (erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 
 if __name__ == "__main__":
